e11ba20f67c8b2422f40906308655b4998365fb4
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-05-03 20:21:15 +0530

    project repo

---
+++ src/pages/map/map.ts
@@ -0,0 +1,52 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+
+import { GoogleMap, GoogleMaps ,LatLng } from '@ionic-native/google-maps';
+
+declare var google: any;
+
+@Component({
+  selector: 'page-map',
+  templateUrl: 'map.html'
+})
+export class MapPage {
+  @ViewChild('map') map;
+
+  constructor(public navCtrl: NavController, public platform: Platform,private googleMaps: GoogleMaps) {}
+
+  initJSMaps(mapEle) {
+    new google.maps.Map(mapEle, {
+      center: { lat: 43.071584, lng: -89.380120 },
+      zoom: 16
+    });
+  }
+
+  initNativeMaps(mapEle) {
+    this.map = new GoogleMap(mapEle);
+    mapEle.classList.add('show-map');
+
+    this.googleMaps.isAvailable().then(() => {
+      const position = new LatLng(43.074395, -89.381056);
+      this.map.setPosition(position);
+    });
+  }
+
+  ionViewDidLoad() {
+    let mapEle = this.map.nativeElement;
+
+    if (!mapEle) {
+      console.error('Unable to initialize map, no map element with #map view reference.');
+      return;
+    }
+
+    // Disable this switch if you'd like to only use JS maps, as the APIs
+    // are slightly different between the two. However, this makes it easy
+    // to use native maps while running in Cordova, and JS maps on the web.
+    if (this.platform.is('cordova') === true) {
+      this.initNativeMaps(mapEle);
+    } else {
+      this.initJSMaps(mapEle);
+    }
+  }
+
+}
